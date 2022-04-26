51ec864b61256ee855f660e999beacc87f90cfd0
sandeepin <sandeepin@qq.com>
2022-04-26 23:47:02 +0800

    feat: 菜单项补全

--- src/main/java/me/jfz/swingframework/Main.java
+++ src/main/java/me/jfz/swingframework/Main.java
@@ -9,6 +9,8 @@ import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.Box;
+import javax.swing.ImageIcon;
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -45,33 +47,8 @@ public class Main {
             jf.setIconImages(FlatSVGUtils.createWindowIconImages("/logo.svg"));
 
             // 菜单栏
-            JMenuBar menuBar1 = new JMenuBar();
-
-            // 菜单栏中菜单项
-            JMenu fileMenu = new JMenu();
-            fileMenu.setText("File");
-            fileMenu.setMnemonic('F');
-            JMenuItem newMenuItem = new JMenuItem("123");
-            JMenuItem openMenuItem = new JMenuItem("345");
-            fileMenu.add(newMenuItem);
-            fileMenu.add(openMenuItem);
-            JMenu optionsMenu = new JMenu();
-            optionsMenu.setText("Options");
-            optionsMenu.setMnemonic('O');
-            menuBar1.add(fileMenu);
-            menuBar1.add(optionsMenu);
-
-            // 菜单栏增加用户按钮 靠右
-            FlatButton usersButton = new FlatButton();
-            usersButton.setIcon(new FlatSVGIcon("/users.svg"));
-            usersButton.setButtonType(FlatButton.ButtonType.toolBarButton);
-            usersButton.setFocusable(false);
-            usersButton.addActionListener(e -> JOptionPane.showMessageDialog(null, "Hello User! How are you?", "User",
-                JOptionPane.INFORMATION_MESSAGE));
-            menuBar1.add(Box.createGlue());
-            menuBar1.add(usersButton);
-
-            jf.setJMenuBar(menuBar1);
+            JMenuBar menuBar = new JMenuBar();
+            createMenuBar(jf, menuBar);
 
             // 创建选项卡面板
             final JTabbedPane tabbedPane = new JTabbedPane();
@@ -85,6 +62,109 @@ public class Main {
             jf.setVisible(true);
         });
         System.out.println("JfzSwingFramework poi!");
+    }
+
+    /**
+     * 创建菜单栏
+     *
+     * @param jf JFrame
+     * @param menuBar 菜单栏
+     */
+    private static void createMenuBar(JFrame jf, JMenuBar menuBar) {
+        // 文件
+        JMenu fileMenu = new JMenu();
+        fileMenu.setText("文件");
+        fileMenu.setMnemonic('F');
+        // 文件-导入配置
+        JMenuItem openMenuItem = new JMenuItem();
+        openMenuItem.setText("导入配置...");
+        openMenuItem.setMnemonic('I');
+        fileMenu.add(openMenuItem);
+        // 文件-导出配置
+        JMenuItem saveAsMenuItem = new JMenuItem();
+        saveAsMenuItem.setText("导出配置...");
+        saveAsMenuItem.setMnemonic('S');
+        fileMenu.add(saveAsMenuItem);
+        fileMenu.addSeparator();
+        // 文件-退出
+        JMenuItem exitMenuItem = new JMenuItem();
+        exitMenuItem.setText("退出");
+        exitMenuItem.setMnemonic('X');
+        fileMenu.add(exitMenuItem);
+        menuBar.add(fileMenu);
+
+        // 视图
+        JMenu viewMenu = new JMenu();
+        viewMenu.setText("视图");
+        viewMenu.setMnemonic('V');
+        // 视图-状态栏
+        JCheckBoxMenuItem statusMenuItem = new JCheckBoxMenuItem();
+        statusMenuItem.setText("状态栏");
+        viewMenu.add(statusMenuItem);
+        menuBar.add(viewMenu);
+
+        // 设置
+        JMenu optionsMenu = new JMenu();
+        optionsMenu.setText("设置");
+        optionsMenu.setMnemonic('O');
+        // 设置-主题
+        JMenuItem themeMenuItem = new JMenuItem();
+        themeMenuItem.setText("主题");
+        themeMenuItem.setMnemonic('T');
+        optionsMenu.add(themeMenuItem);
+        // 设置-插件
+        JMenuItem pluginsMenuItem = new JMenuItem();
+        pluginsMenuItem.setText("插件");
+        pluginsMenuItem.setMnemonic('E');
+        optionsMenu.add(pluginsMenuItem);
+        optionsMenu.addSeparator();
+        // 设置-设置
+        JMenuItem optionsMenuItem = new JMenuItem();
+        optionsMenuItem.setText("设置");
+        optionsMenuItem.setMnemonic('P');
+        optionsMenu.add(optionsMenuItem);
+        menuBar.add(optionsMenu);
+
+        // 帮助
+        JMenu helpMenu = new JMenu();
+        helpMenu.setText("帮助");
+        helpMenu.setMnemonic('H');
+        // 帮助-在线帮助
+        JMenuItem onlineMenuItem = new JMenuItem();
+        onlineMenuItem.setText("在线帮助");
+        onlineMenuItem.setMnemonic('E');
+        helpMenu.add(onlineMenuItem);
+        // 帮助-检查更新
+        JMenuItem updateMenuItem = new JMenuItem();
+        updateMenuItem.setText("检查更新");
+        updateMenuItem.setMnemonic('U');
+        helpMenu.add(updateMenuItem);
+        // 帮助-报告错误
+        JMenuItem reportMenuItem = new JMenuItem();
+        reportMenuItem.setText("报告错误");
+        reportMenuItem.setMnemonic('R');
+        helpMenu.add(reportMenuItem);
+        helpMenu.addSeparator();
+        // 帮助-关于
+        JMenuItem aboutMenuItem = new JMenuItem();
+        aboutMenuItem.setText("关于");
+        aboutMenuItem.setMnemonic('A');
+        helpMenu.add(aboutMenuItem);
+        menuBar.add(helpMenu);
+
+        // 用户按钮 靠右
+        FlatButton usersButton = new FlatButton();
+        ImageIcon flatSVGIcon = new FlatSVGIcon("users.svg");
+        usersButton.setIcon(flatSVGIcon);
+        // usersButton.setIcon(new ImageIcon("D:/Jfz/头像/异想家poi(310x310)无手.jpg"));
+        usersButton.setButtonType(FlatButton.ButtonType.toolBarButton);
+        usersButton.setFocusable(false);
+        usersButton.addActionListener(e -> JOptionPane.showMessageDialog(null, "Hello User! How are you?", "User",
+            JOptionPane.INFORMATION_MESSAGE));
+        menuBar.add(Box.createGlue());
+        menuBar.add(usersButton);
+
+        jf.setJMenuBar(menuBar);
     }
 
     /**
